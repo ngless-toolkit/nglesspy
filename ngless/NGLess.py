d92fda598e50447b6fde00490da37e4de845152e
Luis Pedro Coelho <luis@luispedro.org>
2017-10-12 20:28:08 +0200

    ENH Better documentation with examples

--- ngless/NGLess.py
+++ ngless/NGLess.py
@@ -1,3 +1,52 @@
+'''
+# NGLessPy: NGLess as a Python Embedded Language
+
+This is a variation of [NGLess](http://ngless.embl.de) as an embedded language
+in Python, thus enabling processing of next generation data through a Python
+API.
+
+## Example
+
+```python
+    from ngless import NGLess
+
+    sc = NGLess.NGLess('0.0')
+
+    sc.import_('mocat', '0.0')
+    e = sc.env
+
+    e.sample = sc.load_mocat_sample_('testing')
+
+    @sc.preprocess_(e.sample, using='r')
+    def proc(bk):
+        bk.r = sc.substrim_(bk.r, min_quality=25)
+
+    e.mapped = sc.map_(e.sample, reference='hg19')
+    e.mapped = sc.select_(e.mapped, keep_if=['{mapped}'])
+
+    sc.write_(e.mapped, ofile='ofile.sam')
+
+    sc.run()
+```
+
+This is equivalent to the NGLess script
+
+
+    ngless '0.0'
+    
+
+    
+
+    preprocess_(sample) using='r':
+    
+
+    mapped = map(sample, reference='hg19')
+    
+
+    write(mapped, ofile='ofile.sam')
+'''
+
+
 import os
 
 class NGLessExpression(object):
